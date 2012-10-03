17da7399466a91ce4ffecc22a6023b96da02fa5c
Florin Strugariu <bebef_1987@yahoo.com>
2012-10-03 15:30:13 +0300

    remove Created date

--- pages/page.py
+++ pages/page.py
@@ -4,10 +4,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-'''
-Created on Jun 11, 2012
-
-'''
 from unittestzero import Assert
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
