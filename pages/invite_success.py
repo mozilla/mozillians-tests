2b92d2a428ebd1ca513fd0183832f43b92102eae
Andrei Hutusoru <andreihutusoru@P5069.(none)>
2013-06-14 18:10:02 +0300

    Refactor test_invite

--- pages/invite_success.py
+++ pages/invite_success.py
@@ -12,18 +12,8 @@ from pages.base import Base
 
 class InviteSuccess(Base):
 
-    _success_message_head_locator = (By.CSS_SELECTOR, '#main > h1')
-    _success_message_body_locator = (By.CSS_SELECTOR, '#main > p')
-    _invite_another_mozillian_link_locator = (By.CSS_SELECTOR, '#main > p > a')
+    _success_message_locator = (By.CSS_SELECTOR, '.alert.alert-success')
 
     @property
-    def success_message_body(self):
-        return self.selenium.find_element(*self._success_message_body_locator).text
-
-    @property
-    def success_message_header(self):
-        return self.selenium.find_element(*self._success_message_head_locator).text
-
-    @property
-    def is_invite_another_mozillian_link_present(self):
-        return self.is_element_present(*self._invite_another_mozillian_link_locator)
+    def success_message(self):
+        return self.selenium.find_element(*self._success_message_locator).text
