20832a9e6334bee89815f8402657d0ec4d931143
Andrei Hutusoru <andreihutusoru@P5069.(none)>
2014-03-11 14:35:52 +0200

    Groups page

---
+++ pages/groups_page.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+from selenium.webdriver.common.by import By
+from pages.base import Base
+from pages.create_group_page import CreateGroupPage
+
+
+class GroupsPage(Base):
+
+    _create_group_main_button = (By.CLASS_NAME, 'large')
+    _alert_message_locator = (By.CSS_SELECTOR, '.alert-info')
+
+    def click_create_group_main_button(self):
+        self.selenium.find_element(*self._create_group_main_button).click()
+        return CreateGroupPage(self.testsetup)
+
+    def wait_for_alert_message(self):
+        self.wait_for_element_visible(*self._alert_message_locator)
