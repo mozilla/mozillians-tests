22de62d8439b5e7c58e9c8edac56d6cf9296e658
viorelaioia <viorelaioia@gmail.com>
2017-11-16 19:26:48 +0200

    Fix register tests - scroll to recaptcha checkbox (#388)

--- pages/register.py
+++ pages/register.py
@@ -78,6 +78,8 @@ class Register(Base):
     def check_recaptcha(self):
         recaptcha_iframe_locator = (By.CSS_SELECTOR, '.g-recaptcha iframe')
         recaptcha_iframe = self.find_element(*recaptcha_iframe_locator)
+        self.selenium.execute_script("arguments[0].scrollIntoView();", recaptcha_iframe)
+        # To be removed when bug https://bugzilla.mozilla.org/show_bug.cgi?id=1314462 is fixed
         self.selenium.switch_to_frame(recaptcha_iframe)
         self.wait.until(expected.visibility_of_element_located(
             self._recaptcha_checkbox_locator)).click()
